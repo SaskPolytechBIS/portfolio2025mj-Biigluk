6dde26b1698913c3babce831eddce37cb684ea66
Biigluk <Ibrahim2552@Saskpolytech.ca>
2025-12-05 09:07:08 -0600

    GUI

--- EchoServer (1)/EchoServer/EchoServer/src/ClientGUIConsole.java
+++ EchoServer (1)/EchoServer/EchoServer/src/ClientGUIConsole.java
@@ -1,38 +1,42 @@
 
-import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import java.io.*;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 /**
  *
- * @author vuong
+ * @author oksma
  */
 public class ClientGUIConsole extends JFrame implements ChatIF {
 
-    private JButton closeB = new JButton("Close");
-    private JButton openB = new JButton("Open");
+    final public static int DEFAULT_PORT = 5555;
+    ChatClient client;
+
+    private JButton logoutB = new JButton("Logout");
+    private JButton loginB = new JButton("Login");
     private JButton sendB = new JButton("Send");
     private JButton quitB = new JButton("Quit");
+
     private JTextField portTxF = new JTextField("5555");
     private JTextField hostTxF = new JTextField("127.0.0.1");
     private JTextField messageTxF = new JTextField("");
+
     private JLabel portLB = new JLabel("Port: ", JLabel.RIGHT);
     private JLabel hostLB = new JLabel("Host: ", JLabel.RIGHT);
     private JLabel messageLB = new JLabel("Message: ", JLabel.RIGHT);
+
     private JTextArea messageList = new JTextArea();
 
     public ClientGUIConsole(String host, int port) {
         super("Simple Chat GUI");
         setSize(300, 400);
+
         setLayout(new BorderLayout(5, 5));
         JPanel bottom = new JPanel();
         add("Center", messageList);
         add("South", bottom);
+
         bottom.setLayout(new GridLayout(5, 2, 5, 5));
         bottom.add(hostLB);
         bottom.add(hostTxF);
@@ -40,24 +44,71 @@ public class ClientGUIConsole extends JFrame implements ChatIF {
         bottom.add(portTxF);
         bottom.add(messageLB);
         bottom.add(messageTxF);
-        bottom.add(openB);
+        bottom.add(loginB);
         bottom.add(sendB);
-        bottom.add(closeB);
+        bottom.add(logoutB);
         bottom.add(quitB);
+
         setVisible(true);
-        sendB.addActionListener((ActionEvent e) -> {
-            //send();
-            display(messageTxF.getText() + "\n");
+
+        sendB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                send(messageTxF.getText() + "\n");
+//                display(messageTxF.getText() + "\n");
+            }
         });
+
+        logoutB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Logout();
+            }
+        });
+
+        loginB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                login(messageTxF.getText() + "\n");
+//                display(messageTxF.getText() + "\n");
+            }
+        });
+
+        try {
+            client = new ChatClient(host, port, this);
+
+        } catch (IOException exception) {
+            System.out.println("cannot connect....");
+        }
     }
 
-    @Override
+    public void login(String message) {
+        client.handleMessageFromClientUI("#login " + message);
+    }
+
+    public void Logout() {
+        client.handleMessageFromClientUI("#logoff");
+
+    }
+
+    public void send(String message) {
+        client.handleMessageFromClientUI(message);
+    }
+
     public void display(String message) {
         messageList.insert(message, 0);
     }
 
-    public static void main (String[] args){
-        ClientGUIConsole console = new ClientGUIConsole("localhost", 5555);
-        
+    public static void main(String[] args) {
+        String host = "";
+        int port = 0;
+        try {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            host = "localhost";
+            port = DEFAULT_PORT;
+        }
+        ClientGUIConsole gui = new ClientGUIConsole("localhost", 555);
+        // TODO code application logic here
     }
+
 }
